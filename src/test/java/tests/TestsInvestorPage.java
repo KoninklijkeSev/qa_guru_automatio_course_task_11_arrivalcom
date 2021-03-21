d6d5c80449c00fb045c1473b84674cf55d9d81ef
KoninklijkeSev <strong.m.m@mail.ru>
2021-03-21 19:54:26 +0300

    Delete tag "Desable"

--- src/test/java/tests/TestsInvestorPage.java
+++ src/test/java/tests/TestsInvestorPage.java
@@ -1,7 +1,6 @@
 package tests;
 
 import com.codeborne.pdftest.PDF;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
